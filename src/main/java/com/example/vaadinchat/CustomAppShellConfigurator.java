2c76df05910300c256c92925bba5fb8b2c1840e7
Tsumi <lomakinadiana2016@Gail.com>
2023-12-02 21:56:30 +0700

    first

---
+++ src/main/java/com/example/vaadinchat/CustomAppShellConfigurator.java
@@ -0,0 +1,9 @@
+package com.example.vaadinchat;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+
+@Push
+public class CustomAppShellConfigurator  implements AppShellConfigurator {
+
+}
---
+++ src/test/java/com/example/vaadinchat/VaadinchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaadinchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
